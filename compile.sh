47e7a8a85f922b44eb321a678ec75ddd7200e9df
Horea Christian <horea.christ@yandex.com>
2018-08-30 00:00:39 +0200

    Compile workflow

---
+++ compile.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+TARGET="${1}"
+WHITELIST="
+	article.tex
+	poster.tex
+	pres.tex
+	"
+
+if [ $TARGET = "all" ]; then
+	for ITER_TARGET in *.tex; do
+		if [[ $WHITELIST =~ (^|[[:space:]])$ITER_TARGET($|[[:space:]]) ]];then
+			ITER_TARGET=${ITER_TARGET%".tex"}
+			./compile.sh ${ITER_TARGET}
+		fi
+	done
+else
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	pythontex.py ${TARGET}.tex &&\
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	bibtex ${TARGET} &&\
+	pdflatex -shell-escape ${TARGET}.tex &&\
+	pdflatex -shell-escape ${TARGET}.tex
+fi
