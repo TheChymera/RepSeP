fb4733b704102ccbad811cdf0d86ad1a7fac20cf
Horea Christian <chr@chymera.eu>
2020-06-22 10:19:37 -0400

    Improved bsc_percentage figure style

--- scripts/bsc_percentage.py
+++ scripts/bsc_percentage.py
@@ -14,10 +14,10 @@ fig, ax = plt.subplots(1, 1)
 
 # These are the colors that will be used in the plot
 ax.set_prop_cycle(color=[
-    '#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a',
-    '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94',
-    '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d',
-    '#17becf', '#9edae5'])
+	'#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a',
+	'#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94',
+	'#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d',
+	'#17becf', '#9edae5'])
 
 # Remove the plot frame lines. They are unnecessary here.
 ax.spines['top'].set_visible(False)
@@ -42,56 +42,60 @@ ax.set_yticks(range(0, 91, 10))
 ax.xaxis.set_major_formatter(plt.FuncFormatter('{:.0f}'.format))
 ax.yaxis.set_major_formatter(plt.FuncFormatter('{:.0f}%'.format))
 
-# Provide tick lines across the plot to help your viewers trace along
-# the axis ticks. Make sure that the lines are light and small so they
-# don't obscure the primary data lines.
-ax.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
-
 # Remove the tick marks; they are unnecessary with the tick lines we just
 # plotted. Make sure your axis ticks are large enough to be easily read.
 # You don't want your viewers squinting to read your plot.
-ax.tick_params(axis='both', which='both', labelsize=14,
-               bottom=False, top=False, labelbottom=True,
-               left=False, right=False, labelleft=True)
+ax.tick_params(
+	axis='both',
+	which='both',
+	bottom=False,
+	top=False,
+	labelbottom=True,
+	left=False,
+	right=False,
+	labelleft=True,
+	)
 
 # Now that the plot is prepared, it's time to actually plot the data!
 # Note that I plotted the majors in order of the highest % in the final year.
 majors = ['Health Professions', 'Public Administration', 'Education',
-          'Psychology', 'Foreign Languages', 'English',
-          'Communications\nand Journalism', 'Art and Performance', 'Biology',
-          'Agriculture', 'Social Sciences and History', 'Business',
-          'Math and Statistics', 'Architecture', 'Physical Sciences',
-          'Computer Science', 'Engineering']
+	'Psychology', 'Foreign Languages', 'English',
+	'Communications\nand Journalism', 'Art and Performance', 'Biology',
+	'Agriculture', 'Social Sciences and History', 'Business',
+	'Math and Statistics', 'Architecture', 'Physical Sciences',
+	'Computer Science', 'Engineering']
 
-y_offsets = {'Foreign Languages': 0.5, 'English': -0.5,
-             'Communications\nand Journalism': 0.75,
-             'Art and Performance': -0.25, 'Agriculture': 1.25,
-             'Social Sciences and History': 0.25, 'Business': -0.75,
-             'Math and Statistics': 0.75, 'Architecture': -0.75,
-             'Computer Science': 0.75, 'Engineering': -0.25}
+y_offsets = {
+	'Foreign Languages': 2.25,
+	'English': 0.5,
+	'Communications\nand Journalism': 0.0,
+	'Art and Performance': -1.75,
+	'Biology': -2.0,
+	'Agriculture': 2.0,
+	'Social Sciences and History': 0.15,
+	'Business': -1.5,
+	'Math and Statistics': 0.5,
+	'Architecture': -1.75,
+	'Physical Sciences': -1.75,
+	'Computer Science': 0.75,
+	'Engineering': -1.0,
+	}
 
 for column in majors:
-    # Plot each line separately with its own color.
-    column_rec_name = column.replace('\n', '_').replace(' ', '_')
+	# Plot each line separately with its own color.
+	column_rec_name = column.replace('\n', '_').replace(' ', '_')
 
-    line, = ax.plot('Year', column_rec_name, data=gender_degree_data,
-                    lw=2.5)
+	line, = ax.plot('Year', column_rec_name, data=gender_degree_data,
+			)
+		   #lw=2.5)
 
-    # Add a text label to the right end of every line. Most of the code below
-    # is adding specific offsets y position because some labels overlapped.
-    y_pos = gender_degree_data[column_rec_name][-1] - 0.5
+	# Add a text label to the right end of every line. Most of the code below
+	# is adding specific offsets y position because some labels overlapped.
+	y_pos = gender_degree_data[column_rec_name][-1] - 0.5
 
-    if column in y_offsets:
-        y_pos += y_offsets[column]
+	if column in y_offsets:
+		y_pos += y_offsets[column]
 
-    # Again, make sure that all labels are large enough to be easily read
-    # by the viewer.
-    ax.text(2011.5, y_pos, column, color=line.get_color())
-
-# Make the title big enough so it spans the entire plot, but don't make it
-# so big that it requires two lines to show.
-
-# Note that if the title is descriptive enough, it is unnecessary to include
-# axis labels; they are self-evident, in this plot's case.
-fig.suptitle("Percentage of Bachelor's degrees conferred to women in "
-             "the U.S.A. by major (1970-2011)", fontsize=18, ha="center")
+	# Again, make sure that all labels are large enough to be easily read
+	# by the viewer.
+	ax.text(2011.5, y_pos, column, color=line.get_color())
