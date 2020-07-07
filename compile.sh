2c642dbdb3e535ed7691a05edd274de7f7a4a196
Horea Christian <chr@chymera.eu>
2020-07-07 10:56:57 -0400

    Oputput messages on errors

--- compile.sh
+++ compile.sh
@@ -8,18 +8,18 @@ WHITELIST="
 	slides.tex
 	"
 
-if [ $TARGET = "all" ] || [ "$TARGET" == "" ]; then
+if [[ "$TARGET" = "all" ]] || [[ "$TARGET" == "" ]]; then
 	for ITER_TARGET in *.tex; do
 		if [[ $WHITELIST =~ (^|[[:space:]])$ITER_TARGET($|[[:space:]]) ]];then
 			ITER_TARGET=${ITER_TARGET%".tex"}
-			./compile.sh ${ITER_TARGET}
+			./compile.sh "${ITER_TARGET}"
 		fi
 	done
 else
-	pdflatex -shell-escape ${TARGET}.tex &&\
-	pythontex.py ${TARGET}.tex &&\
-	pdflatex -shell-escape ${TARGET}.tex &&\
-	bibtex ${TARGET} &&\
-	pdflatex -shell-escape ${TARGET}.tex &&\
-	pdflatex -shell-escape ${TARGET}.tex
+	pdflatex -shell-escape "${TARGET}.tex" || { echo "Initial pdflatex failed"; exit $ERRCODE; }
+	pythontex.py "${TARGET}.tex" || { echo "PythonTeX failed"; exit $ERRCODE; }
+	pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after PythonTeX"; exit $ERRCODE; }
+	bibtex "${TARGET}" || { echo "bibtex failed"; exit $ERRCODE; }
+	pdflatex -shell-escape "${TARGET}.tex" || { echo "pdflatex failed after bibtex"; exit $ERRCODE; }
+	pdflatex -shell-escape "${TARGET}.tex"
 fi
