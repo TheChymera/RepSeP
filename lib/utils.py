9965f645c96982de9ff3578ad10d0300081ffec8
Hendrik_Klug <43926224+Jimmy2027@users.noreply.github.com>
2021-03-08 09:54:58 +0100

    improved docstring
    mode documentation clearer

--- lib/utils.py
+++ lib/utils.py
@@ -95,16 +95,23 @@ def inline_anova(anova,
 
 def bold_max_row_value(df, df_tex: str):
     """
-    Wraps the maximum value for each row with \textbf{} in a df.to_latex() string.
+    Detect numeric values in a pandas dataframe LaTeX string (as produced by df.to_latex()), and highlight the largest
+    value of each row by wrapping it with a LaTeX bold tag (\textbf{}).
+    The dataframe body is selected from the tabular LaTeX environment by extracting the lines between \midrule and
+    \bottomrule.
+
 
     Parameters
     ----------
     df :  pandas.DataFrame
-            Pandas DataFrame object containing at least one column with numerical values.
+            Pandas DataFrame object containing at least one column with only numerical values.
 
     df_tex: string
-                The output of df.to_latex(). The values if this string will be changed such that the maximum values of
-                each row will be bold in the resulting pdf.
+                String as returned by df.to_latex(). The values in this string will be changed such that the maximum
+                values of each row will be bold in the resulting pdf. Note that if the maximum value of a row appears
+                anywhere else in the row, as a value or a string, it will be highlighted as well (see the second
+                example below).
+
     >>> import pandas as pd
     >>> df = pd.DataFrame({'col1': [1, 2, 20], 'col2': [3, 4, 8],'col3': [5, 6, 5]})
     >>> print(bold_max_value(df, df.to_latex()), end="")
@@ -115,6 +122,19 @@ def bold_max_row_value(df, df_tex: str):
     0 &     1 &     3 &     \\textbf{5} \\\\
     1 &     2 &     4 &     \\textbf{6} \\\\
     2 &    \\textbf{20} &     8 &     5 \\\\
+    \\bottomrule
+    \\end{tabular}
+
+
+    >>> df = pd.DataFrame({'col1': [1, 2, 20], 'col2': [3, 4, 8],'col3': [5, 6, 5], 'col4': ['2015.02.04', '2016.03.05', '1912.06.12']})
+    >>> print(bold_max_value(df, df.to_latex()), end="")
+    \\begin{tabular}{lrrrl}
+    \\toprule
+    {} &  col1 &  col2 &  col3 &        col4 \\\\
+    \\midrule
+    0 &     1 &     3 &     \\textbf{5} &  201\\textbf{5}.02.04 \\\\
+    1 &     2 &     4 &     \\textbf{6} &  201\\textbf{6}.03.05 \\\\
+    2 &    \\textbf{20} &     8 &     5 &  1912.06.12 \\\\
     \\bottomrule
     \\end{tabular}
     """
